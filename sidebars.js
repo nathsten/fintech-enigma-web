bee07acfef6f595d04a710422b2d490ff031c1b2
Fintech Enigma <116291737+fintech-enigma@users.noreply.github.com>
2026-01-29 18:00:49 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Porteføljeoversikt',
-      items: ['Portefoljeoversikt/Nordnetportefolje', 'Portefoljeoversikt/Fintech Enigma Tradingalgoritmer', 'Portefoljeoversikt/portefoljesammensetning']
+      items: ['Portefoljeoversikt/Nordnetportefolje', 'Portefoljeoversikt/Fintech Enigma Tradingalgoritmer']
     },
     {
       type: 'category',
